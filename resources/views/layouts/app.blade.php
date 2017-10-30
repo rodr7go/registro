de528229ca191be6a9c1e2295676ade92efda119
Eduardo Ortega <ortegon000@hotmail.com>
2017-10-30 17:12:25 -0600

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,39 +19,44 @@
 
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Registro de prospectos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="{{ route('prospects.index') }}"> Prospectos</a>
-                </li>
-            </ul>
-            <div class="dropdown pull-right">
-                <button class="btn btn-info dropdown-toggle"
-                        type="button" id="dropdownMenu1" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                    {{ Auth::user()->name . ' ' . Auth::user()->last_name }}
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                    {{--<a class="dropdown-item" href="#!">Another action</a>--}}
+    @if (auth()->check())
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Registro de prospectos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="{{ route('prospects.index') }}"> Prospectos</a>
+                    </li>
+                </ul>
+
+                <div class="dropdown pull-right">
+                    <button class="btn btn-info dropdown-toggle"
+                            type="button" id="dropdownMenu1" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                        {{ auth()->user()->name . ' ' . auth()->user()->last_name }}
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                        {{--<a class="dropdown-item" href="#!">Another action</a>--}}
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
-    @yield('content')
+        </nav>
 
+    @endif
+
+    @yield('content')
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
