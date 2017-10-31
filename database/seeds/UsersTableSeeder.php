1d106457713a32baa09015f58db904dc1c9e7a1f
Eduardo Ortega <ortegon000@hotmail.com>
2017-10-31 13:09:42 -0600

    seeder usuarios

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,13 +11,46 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name'         => 'Rodrigo',
-            'last_name'    => 'lopez',
-            'email'        => 'rodrigo@controlmas.mx',
-            'job_position' => 'Desarrollo web y mas',
-            'password'     => bcrypt('123456')
-        ]);
+        $users = [
+            [
+                'name'         => 'Rodrigo',
+                'last_name'    => 'lopez',
+                'email'        => 'rodrigo@controlmas.mx',
+                'job_position' => 'Desarrollo web y mas',
+                'password'     => bcrypt('123456')
+            ],
+            [
+                'name'         => 'Eduardo',
+                'last_name'    => 'Ortega',
+                'email'        => 'eduardo@controlmas.mx',
+                'job_position' => 'Desarrollo web y mas',
+                'password'     => bcrypt('123456')
+            ],
+            [
+                'name'         => 'Luis',
+                'last_name'    => 'Ortiz',
+                'email'        => 'luis@controlmas.mx',
+                'job_position' => 'Ventas',
+                'password'     => bcrypt('123456')
+            ],
+            [
+                'name'         => 'Jorge',
+                'last_name'    => 'Rodriguez',
+                'email'        => 'jorge@controlmas.mx',
+                'job_position' => 'Marketing',
+                'password'     => bcrypt('123456')
+            ],
+            [
+                'name'         => 'Cesar',
+                'last_name'    => 'Ortega',
+                'email'        => 'cesarortega@controlmas.mx',
+                'job_position' => 'Diseño Grafico',
+                'password'     => bcrypt('123456')
+            ],
+
+        ];
+
+        \DB::table('users')->insert($users);
     }
 
 }
