985e3f8a3b247b227aa6184192f287b5a2dabc03
Eduardo Ortega <ortegon000@hotmail.com>
2017-10-31 13:48:05 -0600

    campos nuevos prospectos

--- database/migrations/2017_10_29_231623_create_prospects_table.php
+++ database/migrations/2017_10_29_231623_create_prospects_table.php
@@ -15,16 +15,27 @@ class CreateProspectsTable extends Migration
     {
         Schema::create('prospects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('website');
-            $table->string('url');
-            $table->string('address');
-            $table->string('order');
-            $table->string('email');
+            $table->string('name');
+            $table->string('website')->nullable();
+            $table->string('contact_name')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('address')->nullable();
+            $table->string('order')->nullable();
             $table->string('phone');
-            $table->string('contact_name');
-            $table->string('comments');
-            $table->string('tracing');
-            $table->string('status');
+            $table->text('comments');
+            $table->text('sale_proposal');
+            $table->text('tracing')->nullable();
+            $table->boolean('is_client')->default(0);
+
+            $table->dateTime('alert_date')->nullable(); 
+            $table->string('alert_subject')->nullable();
+
+            $table->unsignedInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+
+            $table->unsignedInteger('updated_by');
+            $table->foreign('updated_by')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
@@ -36,6 +47,6 @@ class CreateProspectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('prospects');
+        Schema::dropIfExists('prospects');  
     }
 }
